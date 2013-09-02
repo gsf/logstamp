d75646d3c0b2260b1b4d7fb9f4b8cde48260c064
Gabriel Farrell <g@grrawr.com>
2013-09-02 15:15:28 -0400

    Missed in the rename

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function (console_) {
   var log = console_.log;
-  console_.log = cnsl.info = function () {
+  console_.log = console_.info = function () {
     module.exports.stamp(console_._stdout);
     log.apply(console_, arguments);
   };
 
   var warn = console_.warn;
-  console_.warn = cnsl.error = function () {
+  console_.warn = console_.error = function () {
     module.exports.stamp(console_._stderr);
     warn.apply(console_, arguments);
   };
