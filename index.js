2b802b92472875172ec50a685b4c2fa21acd9592
Gabriel Farrell <g@grrawr.com>
2013-12-28 07:18:05 -0500

    Refactored to remove repetition

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 var stream = require('stream');
 
-var outStream = new stream.Writable({decodeStrings: false});
-var errStream = new stream.Writable({decodeStrings: false});
-
 module.exports = function (stamp) {
-  var stdout = console._stdout;
-  var stderr = console._stderr;
-
   stamp = stamp || function () {
     return new Date().toISOString() + ' ';
   };
 
-  outStream._write = function (chunk, encoding, callback) {
-    stdout.write(stamp());
-    stdout.write(chunk);
-    callback();
-  };
+  var std = {};
 
-  errStream._write = function (chunk, encoding, callback) {
-    stderr.write(stamp());
-    stderr.write(chunk);
-    callback();
-  };
-
-  console._stdout = outStream;
-  console._stderr = errStream;
+  ['out', 'err'].forEach(function (s) {
+    std[s] = console['_std' + s];
+    console['_std' + s] = new stream.Writable({decodeStrings: false});
+    console['_std' + s]._write = function (chunk, encoding, callback) {
+      std[s].write(stamp());
+      std[s].write(chunk);
+      callback();
+    };
+  });
 };
